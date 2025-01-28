e7f0b29aeb7277cf505e02e7743ccf6bcd514855
rcmcphee <rcmcphee34@gmail.com>
2025-01-28 18:50:05 -0500

    Updated debugging program with bubble sort

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -28,8 +28,23 @@ def recombine(leftArr, rightArr):
 
     return mergeArr
 
+def bubble_sort(arr):
+    n = len(arr)
+    
+    # Traverse through all elements in the array
+    for i in range(n):
+        # Last i elements are already sorted
+        for j in range(0, n-i-1):
+            
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j-1]:
+                arr[j], arr[j+1] = arr[j-1], arr[j]
+    
+    return arr
+
 arr = rand.random_array([None] * 20)
 arr_out = mergeSort(arr)
+arr_out_bubble = bubble_sort(arr)
 
 print(arr_out)
 
