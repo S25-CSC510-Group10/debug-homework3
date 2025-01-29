a417222f123c8d3047c1ca32ff79eb4419cf7902
rcmcphee <rcmcphee34@gmail.com>
2025-01-28 19:25:09 -0500

    Pylint debugging before and after reports and edited python source files

--- rand.py
+++ rand.py
@@ -1,8 +1,10 @@
+"""Import the subprocess package to use subprocess functions"""
 import subprocess
 
 def random_array(arr):
+    """Function randomizing an array"""
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+    for i in enumerate(arr):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
         arr[i] = int(shuffled_num.stdout)
     return arr
