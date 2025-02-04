52f436fabb96425eb704e4fd71711908b1cd84cc
Lawrence <lasteph2@ncsu.edu>
2025-02-04 16:12:56 -0500

    Pushing code with issues

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,54 +1,82 @@
+"""
+Module for using mergesort and bogosort
+"""
+
 import rand
+import random
 
 
-def mergeSort(arr):
+def merge_sort(arr):
+    """
+    Recursively sorts an array using Merge Sort.
+
+    :param array: List of elements to be sorted
+    :return: Sorted list
+    """
     if len(arr) == 1:
         return arr
 
     half = len(arr) // 2
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
 
 
-def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+def recombine(left_arr, right_arr):
+    """
+    Merges two sorted subarrays into one sorted array.
+
+    :param left_arr: Left sorted subarray
+    :param right_arr: Right sorted subarray
+    :return: Merged sorted array
+    """
+    left_index = 0
+    right_index = 0
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            right_index += 1
+            merge_arr[left_index + right_index] = left_arr[left_index]
         else:
-            leftIndex += 1
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+            left_index += 1
+            merge_arr[left_index + right_index] = right_arr[right_index]
 
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
+    for i in range(right_index, len(right_arr)):
+        merge_arr[left_index + right_index] = right_arr[i]
 
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+    for i in range(left_index, len(left_arr)):
+        merge_arr[left_index + right_index] = left_arr[i]
 
-    return mergeArr
-
-
-arr = rand.random_array([None] * 20)
-arr_out = mergeSort(arr)
-
-print(arr_out)
-
-import random
+    return merge_arr
 
 
-def Bogosort(list):
-    while isSorted(list):
-        random.shuffle(list)
-    return list
+def bogosort(list2):
+    """
+    Sorts a list using the highly inefficient Bogosort algorithm.
+
+    :param array: List of elements to be sorted
+    :return: Sorted list
+    """
+    while is_sorted(list2):
+        random.shuffle(list2)
+    return list2
 
 
-def isSorted(list):
+def is_sorted(list2):
+    """
+    Checks if the list is sorted.
+
+    :param array: List to check
+    :return: True if sorted, False otherwise
+    """
     prev = None
-    for x in list:
-        if prev != None and (prev > x):
+    for x in list2:
+        if prev is not None and (prev > x):
             return False
         prev = x
     return True
+
+
+arr1 = rand.random_array([None] * 20)
+arr_out = merge_sort(arr1)
+arr_bogo = bogosort(arr1)
+print(arr_out)
--- rand.py
+++ rand.py
@@ -1,8 +1,17 @@
+"""
+Module for generating random arrays.
+"""
+
 import subprocess
 
+
 def random_array(arr):
-    shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+    """
+    Returns a random array.
+    """
+    for i, _ in enumerate(arr):
+        shuffled_num = subprocess.run(
+            ["shuf", "-i1-20", "-n1"], capture_output=True, text=True, check=True
+        )
+        arr[i] = int(shuffled_num.stdout.strip())
     return arr
