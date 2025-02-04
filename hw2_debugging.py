88b4e4fa23497c7218c77f982c0be4ef3f49f77e
Lawrence <lasteph2@ncsu.edu>
2025-02-04 15:33:45 -0500

    Added buggy code for step 3

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,12 +1,14 @@
 import rand
 
+
 def mergeSort(arr):
-    if (len(arr) == 1):
+    if len(arr) == 1:
         return arr
 
-    half = len(arr)//2
+    half = len(arr) // 2
 
     return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+
 
 def recombine(leftArr, rightArr):
     leftIndex = 0
@@ -22,15 +24,31 @@ def recombine(leftArr, rightArr):
 
     for i in range(rightIndex, len(rightArr)):
         mergeArr[leftIndex + rightIndex] = rightArr[i]
-    
+
     for i in range(leftIndex, len(leftArr)):
         mergeArr[leftIndex + rightIndex] = leftArr[i]
 
     return mergeArr
+
 
 arr = rand.random_array([None] * 20)
 arr_out = mergeSort(arr)
 
 print(arr_out)
 
+import random
 
+
+def Bogosort(list):
+    while isSorted(list):
+        random.shuffle(list)
+    return list
+
+
+def isSorted(list):
+    prev = None
+    for x in list:
+        if prev != None and (prev > x):
+            return False
+        prev = x
+    return True
